dc58cb6aa19515446e8a81073837b957f669d603
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2022-05-24 21:22:37 +0300

    Add a pins, move of pin, image with weather's description and russian localizate

--- WeatherOfEntryCountryRestApi/WeatherOfEntryCountryRestApi/Constants.cs
+++ WeatherOfEntryCountryRestApi/WeatherOfEntryCountryRestApi/Constants.cs
@@ -6,6 +6,6 @@ namespace WeatherOfEntryCountryRestApi
 {
     public static class Constants
     {
-        public static string RestUrl = "https://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&appid=fa88c9cdfdb5bff9bc0b42893067a148";
+        public static string RestUrl = "https://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&appid=fa88c9cdfdb5bff9bc0b42893067a148&lang=ru";
     }
 }
--- WeatherOfEntryCountryRestApi/WeatherOfEntryCountryRestApi/View/MainPage.xaml.cs
+++ WeatherOfEntryCountryRestApi/WeatherOfEntryCountryRestApi/View/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace WeatherOfEntryCountryRestApi.View
 {
@@ -16,6 +17,7 @@ namespace WeatherOfEntryCountryRestApi.View
             InitializeComponent();
         }
 
+        //http://openweathermap.org/img/w/04d.png
         private async void CitiesNamesSearchBar_SearchButtonPressed(object sender, EventArgs e)
         {
             try
@@ -27,12 +29,27 @@ namespace WeatherOfEntryCountryRestApi.View
                 PressureLbl.Text = a.main.pressure.ToString();
                 HumidityLbl.Text = a.main.humidity.ToString();
                 WindLbl.Text = a.wind.speed.ToString();
-
+                qqww.Text = a.weather[0].description;
+                Pin pin = new Pin
+                {
+                    Label = a.name,
+                    Address = a.weather.Rank.ToString(),
+                    Type = PinType.Place,
+                    Position = new Position(a.coord.lat, a.coord.lon)
+                };
+                MyMap.Pins.Add(pin);
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(pin.Position, Distance.FromMeters(5000)));
+                weatherImg.Source = ImageSource.FromUri(new Uri($"http://openweathermap.org/img/w/{a.weather[0].icon}.png"));
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Error", $"Введите название города\n{ex.Message}", "Ok");
             }
+        }
+
+        private void MyMap_MapClicked(object sender, MapClickedEventArgs e)
+        {
+
         }
     }
 }
